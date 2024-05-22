f49b242b4da77c14df3dfe62679da50928fd0a26
It's me <74585420+cmp9797@users.noreply.github.com>
2024-05-22 22:56:48 +0700

    configure alamofire

--- TheMealsApp/TheMealsApp/Core/Data/Remote/RemoteDataSource.swift
+++ TheMealsApp/TheMealsApp/Core/Data/Remote/RemoteDataSource.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 protocol RemoteDataSourceProtocol: AnyObject {
 
@@ -24,25 +25,19 @@ final class RemoteDataSource: NSObject {
 extension RemoteDataSource: RemoteDataSourceProtocol {
 
   func getCategories(
-    result: @escaping (Result<[CategoryResponse], URLError>) -> Void
-  ) {
-    guard let url = URL(string: Endpoints.Gets.categories.url) else { return }
-
-    let task = URLSession.shared.dataTask(with: url) { maybeData, maybeResponse, maybeError in
-      if maybeError != nil {
-        result(.failure(.addressUnreachable(url)))
-      } else if let data = maybeData, let response = maybeResponse as? HTTPURLResponse, response.statusCode == 200 {
-        let decoder = JSONDecoder()
-        do {
-          let categories = try decoder.decode(CategoriesResponse.self, from: data).categories
-          result(.success(categories))
-        } catch {
-          result(.failure(.invalidResponse))
-        }
-      }
-    }
-    task.resume()
-
-  }
+     result: @escaping (Result<[CategoryResponse], URLError>) -> Void
+   ) {
+     guard let url = URL(string: Endpoints.Gets.categories.url) else { return }
+  
+     /// use alamofire to get data from url
+     AF.request(url).validate().responseDecodable(of: CategoriesResponse.self) { response in
+       switch response.result {
+       case .success(let value):
+         result(.success(value.categories))
+       case .failure:
+         result(.failure(.invalidResponse))
+       }
+     }
+   }
 
 }
